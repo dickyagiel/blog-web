7105b18fbd96c5d8cdaa76095f82ca6260b94632
unknown <ldickyagiels@gmail.com>
2025-10-03 13:42:05 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About | Belajar Laravel</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <script src="js/script.js"></script>
+    <h1>About {{ $name }}</h1>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+<hr>
+    <img src="img/tes.jpg" alt="sky" width="300">
+</body>
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog | Belajar Laravel</title>
+</head>
+<body>
+    
+    <h1>myBlog</h1>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <br>
+    <h3>Today News</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas perspiciatis laboriosam voluptates adipisci repellat officia dolorum ex quo, atque necessitatibus, doloremque mollitia explicabo! Eos fuga temporibus impedit, excepturi quasi voluptatem?</p>
+    <h3>Yesterday News</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic sapiente nihil quos vero eligendi natus voluptatem aperiam ut, aspernatur necessitatibus fugiat nam quis expedita deserunt saepe? Quos alias ex assumenda?</p>
+
+</body>
+</html>
